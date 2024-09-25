a0728e28a11aaf9c4a9893d5a6abff3f187dd2a9
OrionPaX4k <thomas.lebouc@epitech.eu>
2024-09-25 11:53:14 +0200

    [ADD] new lib file named querier

---
+++ app/front/web/src/views/profil/ProfilPage.vue
@@ -0,0 +1,16 @@
+<!--
+/////// Account Settings \\\\\\\
+------------ Profil ------------
+    * Photo de profil *
+
+------------ Account ------------
+    * Username *
+    * Password *
+    * Email *
+    * Time Zone *
+
+------------ Linked Accounts ------------
+    * Github Connection *
+    * Google Connection *
+\\\\\\\ Update Button ///////
+-->
