b3855633aa3b396d6bc87e4b7c6fed633fcc44ec
Harleen <harleen.singh-kaur@epitech.eu>
2024-10-02 00:38:50 +0200

    [ADD] Nagivation between all screens, add a path for Sign in to forgot password, a link to Sign In and Sign Up screens

---
+++ app/front/mobile/src/Navigation/index.tsx
@@ -0,0 +1,31 @@
+import  React from 'react'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import SignIn from '../screens/Authentication/SignIn/signIn';
+import SignUp from '../screens/Authentication/SignUp';
+import ConfirmEmail from '../screens/Authentication/ConfirmEmail/ConfirmEmail';
+import ForgotPassword from '../screens/Authentication/ForgotPassword';
+import NewPassword from '../screens/Authentication/ForgotPassword/newPassword';
+import Home from '../screens/Home';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Sign Up" component={SignUp} />
+                <Stack.Screen name="Sign In" component={SignIn} />
+                <Stack.Screen name="Confirmation email" component={ConfirmEmail} />
+                <Stack.Screen name="Forgot password" component={ForgotPassword} />
+                <Stack.Screen name="New password" component={NewPassword} />
+                
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Navigation
