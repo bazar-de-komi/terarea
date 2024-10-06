511667954675425d33188ab522a40ca95c44e714
Harleen <harleen.singh-kaur@epitech.eu>
2024-10-06 19:11:13 +0200

    [UPDATE] New password page and add link between reset password and new password screens

--- app/front/mobile/src/screens/Authentication/ForgotPassword/newPassword.tsx
+++ app/front/mobile/src/screens/Authentication/ForgotPassword/newPassword.tsx
@@ -1,84 +1,89 @@
 import React, {useState} from "react";
-import { View, Text, Image, StyleSheet, ScrollView} from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView, useWindowDimensions} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 import CustomerInput from "../../../components/CustomersInput/CustomerInput";
 import CustomerButton from "../../../components/CustomerButton";
-import { useNavigation } from "@react-navigation/native";
+
+import IfttLogo from '../../../../assets/authenticationLogo/font-style.png';
 
 const NewPassword = () => {
     const [code, setCode] = useState('');
     const [newPassword, setNewPassword] = useState('');
+    const { height } = useWindowDimensions();
     const Navigation = useNavigation();
     
     const SignSubmitPressed = () => {
-        console.warn("Submit");
+        Navigation.navigate("Sign In");
     }
 
     const loginPressed = () => {
-        console.warn("Log in");
         Navigation.navigate("Sign In");
     }
 
     return (
     <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.NewPasswordContainer}>
-            <Text style={styles.NewPasswordTitle}>Add New password</Text>
-            <CustomerInput
-            placeholder="code"
-            value={code}
-            setValue={setCode}
-            secureTextEntry={false}
+        <View style={styles.bgNewForgotPass}>
+        <Image
+                    source={IfttLogo}
+                    style={[styles.IftttLogo, { height: height * 0.1 }]}
+                    resizeMode="contain"
             />
-            <CustomerInput
-            placeholder="Enter your new password"
-            value={newPassword}
-            setValue={setNewPassword}
-            secureTextEntry={false}
-            />
-            <CustomerButton text="Submit" onPress={SignSubmitPressed} bgColor={""} fgColor={""}/>
+                <View style={styles.NewPasswordContainer}>
+                    <Text style={styles.NewPasswordTitle}>Add New password</Text>
+                    <CustomerInput
+                    placeholder="Enter you verification code"
+                    value={code}
+                    setValue={setCode}
+                    secureTextEntry={false}
+                    />
+                    <CustomerInput
+                    placeholder="Enter your new password"
+                    value={newPassword}
+                    setValue={setNewPassword}
+                    secureTextEntry={false}
+                    />
+                    <CustomerButton text="Submit" onPress={SignSubmitPressed} bgColor={""} fgColor={""}/>
 
-            <CustomerButton
-            text="Back to Sign in"
-            onPress={loginPressed}
-            type="TERTIARY"
-            bgColor={""}
-            fgColor={""}
-            />
-
+                    <CustomerButton
+                    text="Back to Sign in"
+                    onPress={loginPressed}
+                    type="TERTIARY"
+                    bgColor={""}
+                    fgColor={""}
+                    />
         </View>
+                </View>
     </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    NewPasswordContainer: {
+    bgNewForgotPass: {
+        top: 20,
+        marginTop: 20,
+        justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
+        backgroundColor: '#F5F5F5',
+    },
+    NewPasswordContainer: {
+        width: '100%',
+        maxWidth: 400,
+        backgroundColor: '#e0d8d7',
+        borderRadius: 15,
+        alignItems: 'center',
+        padding: 5,
     },
     IftttLogo: {
         width: '40%',
         maxWidth: 300,
-        maxHeight: 200,
-    },
-    logoButtonContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: '80%',
-        // backgroundColor: '#FAE9EA',
-        borderRadius: 5,
-        padding: 10,
-        marginVertical: 5,
-    },
-    logo: {
-        right: 80,
-        top: 40,
-        width: 24,
-        height: 24,
-        marginRight: 10,
+        maxHeight: 100,
+        marginBottom: 60,
     },
     NewPasswordTitle: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: '#051C60',
         margin: 10,
     },
 })
