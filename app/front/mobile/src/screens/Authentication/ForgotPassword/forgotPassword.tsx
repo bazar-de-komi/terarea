645c3757615cd5bbfb2f6651e954f879525611c2
Harleen <harleen.singh-kaur@epitech.eu>
2024-10-06 18:55:07 +0200

    [ADD] link with New password screens

--- app/front/mobile/src/screens/Authentication/ForgotPassword/forgotPassword.tsx
+++ app/front/mobile/src/screens/Authentication/ForgotPassword/forgotPassword.tsx
@@ -14,6 +14,7 @@ const ForgotPassword = () => {
 
     const SignSendPressed = () => {
         console.warn("send");
+        navigation.navigate("New password");
     }
 
     const loginPressed = () => {
