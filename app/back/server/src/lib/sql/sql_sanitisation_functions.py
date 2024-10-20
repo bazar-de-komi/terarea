bfc4e18cb86f30776aec985d80283e59508fdc98
Henry LETELLIER <henry.letellier@epitech.eu>
2024-10-21 01:23:52 +0200

    [ADD] a function in charge of protecting the where value

--- app/back/server/src/lib/sql/sql_sanitisation_functions.py
+++ app/back/server/src/lib/sql/sql_sanitisation_functions.py
@@ -101,6 +101,62 @@ class SQLSanitiseFunctions:
             return data[0]
         return columns
 
+    def _protect_value(self, value: str) -> str:
+        """Ensures the value is safely passed as a string in an SQL query.
+        It wraps the value in single quotes and escapes any single quotes within the value.
+
+        Args:
+            value (str): The value that needs to be protected.
+
+        Returns:
+            str: The value with protection applied, safe for SQL queries.
+        """
+        title = "_protect_value"
+        self.disp.log_debug(f"protecting value: {value}", title)
+        if value is None:
+            self.disp.log_debug("Value is none, thus returning NULL", title)
+            return "NULL"
+
+        if isinstance(value, str) is False:
+            self.disp.log_debug("Value is not a string, converting", title)
+            value = str(value)
+
+        if len(value) == 0:
+            self.disp.log_debug("Value is empty, returning ''", title)
+            return "''"
+
+        if value[0] == '`' and value[-1] == '`':
+            self.disp.log_debug(
+                "string has special backtics, skipping.", title
+            )
+            return value
+
+        if value[0] == "'":
+            self.disp.log_debug(
+                "Value already has a single quote at the start, removing", title
+            )
+            value = value[1:]
+        if value[-1] == "'":
+            self.disp.log_debug(
+                "Value already has a single quote at the end, removing", title
+            )
+            value = value[:-1]
+
+        self.disp.log_debug(
+            f"Value before quote escaping: {value}", title
+        )
+        protected_value = value.replace("'", "''")
+        self.disp.log_debug(
+            f"Value after quote escaping: {protected_value}", title
+        )
+
+        protected_value = f"'{protected_value}'"
+        self.disp.log_debug(
+            f"Value after being converted to a string: {protected_value}.",
+            title
+        )
+        return protected_value
+
     def escape_risky_column_names_where_mode(self, columns: Union[List[str], str]) -> Union[List[str], str]:
         """
         Escape the risky column names in where mode, except for those in keyword_logic_gates.
@@ -113,8 +169,7 @@ class SQLSanitiseFunctions:
         """
         title = "_escape_risky_column_names_where_mode"
         self.disp.log_debug(
-            "Escaping risky column names in where mode.",
-            title
+            "Escaping risky column names in where mode.", title
         )
 
         if isinstance(columns, str):
@@ -127,19 +182,22 @@ class SQLSanitiseFunctions:
                 key, value = item.split("=", maxsplit=1)
                 self.disp.log_debug(f"key = {key}, value = {value}", title)
 
+                protected_value = self._protect_value(value)
                 if key.lower() not in self.keyword_logic_gates and key.lower() in self.risky_keywords:
                     self.disp.log_warning(
-                        f"Escaping risky column name '{key}'.",
-                        title
+                        f"Escaping risky column name '{key}'.", title
                     )
-                    data[index] = f"\'{key}\'={value}"
+                    data[index] = f"`{key}`={protected_value}"
+                else:
+                    data[index] = f"{key}={protected_value}"
 
             elif item.lower() not in self.keyword_logic_gates and item.lower() in self.risky_keywords:
                 self.disp.log_warning(
                     f"Escaping risky column name '{item}'.",
                     title
                 )
-                data[index] = f"\'{item}\'"
+                protected_value = self._protect_value(item)
+                data[index] = protected_value
 
         self.disp.log_debug("Escaped risky column names in where mode.", title)
 
@@ -222,19 +280,21 @@ class SQLSanitiseFunctions:
         self.disp.log_debug(f"beautified_table = {data}", "_beautify_table")
         return data
 
-    def compile_update_line(self, line: List, column: List, column_length) -> str:
+    def compile_update_line(self, line: List, column: List, column_length: int) -> str:
         """_summary_
             Compile the line required for an sql update to work.
 
         Args:
             line (List): _description_
             column (List): _description_
-            column_length (_type_): _description_
+            column_length (int): _description_
 
         Returns:
             str: _description_
         """
+        title = "compile_update_line"
         final_line = ""
+        self.disp.log_debug("Compiling update line.", title)
         for i in range(0, column_length):
             cell_content = self.check_sql_cell(line[i])
             final_line += f"{column[i]} = {cell_content}"
@@ -242,7 +302,7 @@ class SQLSanitiseFunctions:
                 final_line += ", "
             if i == column_length:
                 break
-        self.disp.log_debug(f"line = {final_line}", "_compile_update_line")
+        self.disp.log_debug(f"line = {final_line}", title)
         return final_line
 
     def process_sql_line(self, line: List[str], column: List[str], column_length: int = (-1)) -> str:
