26a73d3ba027ae224df8084ef455f2adba93c33a
Harleen <harleen.singh-kaur@epitech.eu>
2024-10-22 12:03:49 +0200

    [EDIT] nagivation page for add all, services, applets ans applets screens

--- app/front/mobile/src/Navigation/index.tsx
+++ app/front/mobile/src/Navigation/index.tsx
@@ -1,5 +1,4 @@
 import  React from 'react'
-import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
@@ -9,6 +8,11 @@ import ConfirmEmail from '../screens/Authentication/ConfirmEmail/ConfirmEmail';
 import ForgotPassword from '../screens/Authentication/ForgotPassword';
 import NewPassword from '../screens/Authentication/ForgotPassword/newPassword';
 import Home from '../screens/Home/home';
+import All from '../screens/Home/all';
+import Applets from '../screens/Home/applets';
+import Services from '../screens/Home/services';
+import Start from '../screens/Start/start';
+import AppletsScreen from '../screens/AppletsScreen/AppletsScreen';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +20,7 @@ const Navigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Start" component={Start} />
                 <Stack.Screen name="Sign Up" component={SignUp} />
                 <Stack.Screen name="Sign In" component={SignIn} />
                 <Stack.Screen name="Confirmation email" component={ConfirmEmail} />
@@ -23,6 +28,11 @@ const Navigation = () => {
                 <Stack.Screen name="New password" component={NewPassword} />
                 
                 <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="All" component={All} />
+                <Stack.Screen name="Applets" component={Applets} />
+                <Stack.Screen name="Services" component={Services} />
+
+                <Stack.Screen name="Applets screen" component={AppletsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
