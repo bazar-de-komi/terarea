f41915d780ed86a439a23463e5a72b26f3893fb6
Eric <eric1.xu@epitech.eu>
2024-10-22 11:31:20 +0200

    [ADD] Oauth authentication class to the export file

--- app/back/server/src/lib/components/endpoints/__init__.py
+++ app/back/server/src/lib/components/endpoints/__init__.py
@@ -4,9 +4,11 @@
 from .bonus import Bonus
 from .services import Services
 from .user_endpoints import UserEndpoints
+from .oauth_authentication import OAuthAuthentication
 
 __all__ = [
     "Bonus",
     "Services",
-    "UserEndpoints"
+    "UserEndpoints",
+    "OAuthAuthentication"
 ]
