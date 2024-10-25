9a30d17e5c4ad56a77a26872bb5942066c695b0a
Eric <eric1.xu@epitech.eu>
2024-10-26 00:51:06 +0200

    [FIX] every services getter functions, every functions works now

--- app/back/server/src/lib/components/endpoints/services.py
+++ app/back/server/src/lib/components/endpoints/services.py
@@ -1,5 +1,5 @@
-"""_summary_
-    File containing boilerplate functions that could be used by the server in it's endpoints_initialised for checking incoming data.
+"""
+This file contains every method about services
 """
 
 from fastapi import Response, Request
@@ -10,17 +10,13 @@ from ..http_codes import HCI
 
 
 class Services:
-    """_summary_
+    """
+    The class that contains every method about services
     """
 
     def __init__(self, runtime_data: RuntimeData, success: int = 0, error: int = 84, debug: bool = False) -> None:
         """_summary_
-
-        Args:
-            runtime_data (RuntimeData): _description_
-            success (int, optional): _description_. Defaults to 0.
-            error (int, optional): _description_. Defaults to 84.
-            debug (bool, optional): _description_. Defaults to False.
+        The constructor of the services class
         """
         self.debug: bool = debug
         self.success: int = success
@@ -41,98 +37,158 @@ class Services:
         """
         title = "get_services"
         token = self.runtime_data_initialised.boilerplate_incoming_initialised.get_token_if_present(
-            request)
+            request
+        )
         self.disp.log_debug(f"Token = {token}", title)
-        if token is None:
-            return HCI.unauthorized({"error": "Authorisation required."})
-
+        if not token:
+            body = self.runtime_data_initialised.boilerplate_responses_initialised.build_response_body(
+                title=title,
+                message="Authorisation required.",
+                resp="Unauthorized",
+                token=token,
+                error=True
+            )
+            return HCI.unauthorized(
+                content=body,
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
         services_data = self.runtime_data_initialised.database_link.get_data_from_table(
             CONST.TAB_SERVICES,
             "*"
         )
-        if services_data is None or isinstance(services_data, int):
-            return HCI.not_found({"error": "Services not found."}, content_type=CONST.CONTENT_TYPE, headers=self.runtime_data_initialised.json_header)
-        self.disp.log_debug(f"received in {title}", services_data)
-        return HCI.success({"services": services_data})
+        if not services_data or isinstance(services_data, int):
+            body = self.runtime_data_initialised.boilerplate_responses_initialised.build_response_body(
+                title=title,
+                message="Services not found.",
+                resp="Not found",
+                token=token,
+                error=True
+            )
+            return HCI.not_found(
+                content=body,
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
+        self.disp.log_debug(f"Retrieved data {services_data}", title)
+        for i, service in enumerate(services_data):
+            services_data[i]["created_at"] = self.runtime_data_initialised.database_link.datetime_to_string(service["created_at"])
+        body = self.runtime_data_initialised.boilerplate_responses_initialised.build_response_body(
+                title=title,
+                message=services_data,
+                resp="Success",
+                token=token
+            )
+        return HCI.success(
+            content=body,
+            content_type=CONST.CONTENT_TYPE,
+            headers=self.runtime_data_initialised.json_header
+        )
 
-    async def get_service(self, name, request: Request) -> Response:
-        """_summary_
-
-        Args:
-            name (_type_): _description_
-            request (Request): _description_
-
-        Returns:
-            Response: _description_
+    async def get_service(self, request: Request, name: str) -> Response:
+        """
+        The method to get a service by his name
         """
         title = "get_service"
         token = self.runtime_data_initialised.boilerplate_incoming_initialised.get_token_if_present(
-            request)
+            request
+        )
         self.disp.log_debug(f"Token = {token}", title)
-        if token is None:
-            return HCI.unauthorized({"error": "Authorization required."}, content_type=CONST.CONTENT_TYPE, headers=self.runtime_data_initialised.json_header)
-
+        if not token:
+            return HCI.unauthorized(
+                {"error": "Authorization required."},
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
         service_data = self.runtime_data_initialised.database_link.get_data_from_table(
-            CONST.TAB_SERVICES)
-        service = next(
-            (srv for srv in service_data if srv["name"] == name), None)
-        if service is None:
-            return HCI.not_found({"error": f"Service '{name}' not found."}, content_type=CONST.CONTENT_TYPE, headers=self.runtime_data_initialised.json_header)
-        self.disp.log_debug(f"Service found: {service}", title)
-        return HCI.success({"msg": service})
+            CONST.TAB_SERVICES,
+            "*",
+            f"name='{name}'"
+        )
+        for i, service in enumerate(service_data):
+            service_data[i]["created_at"] = self.runtime_data_initialised.database_link.datetime_to_string(service["created_at"])
+        self.disp.log_debug(f"Service found: {service_data}", title)
+        return HCI.success(
+            {"service": service_data},
+            content_type=CONST.CONTENT_TYPE,
+            headers=self.runtime_data_initialised.json_header
+        )
 
     async def get_services_by_tag(self, tag, request: Request) -> Response:
-        """_summary_
-
-        Args:
-            tag (_type_): _description_
-            request (Request): _description_
-
-        Returns:
-            Response: _description_
+        """
+        The function to get and filter every services by specifics tag
         """
         title = "get_services_by_tag"
         token = self.runtime_data_initialised.boilerplate_incoming_initialised.get_token_if_present(
-            request)
+            request
+        )
         self.disp.log_debug(f"Token = {token}", title)
-        if token is None:
-            return HCI.unauthorized({"error": "Authorization required."})
-
+        if not token:
+            return HCI.unauthorized(
+                {"error": "Authorization required."},
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
         service_data = self.runtime_data_initialised.database_link.get_data_from_table(
-            CONST.TAB_SERVICES)
+            CONST.TAB_SERVICES,
+            "*"
+        )
         filtered_services = [
-            srv for srv in service_data if tag in srv.get("tags", [])]
+            srv for srv in service_data if tag in srv.get("tags", [])
+        ]
         if not filtered_services:
-            return HCI.not_found({"error": f"No services found for tag '{tag}'."}, content_type=CONST.CONTENT_TYPE, headers=self.runtime_data_initialised.json_header)
+            return HCI.not_found(
+                {"error": f"No services found for tag '{tag}'."},
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
+        for i, service in enumerate(filtered_services):
+            filtered_services[i]["created_at"] = self.runtime_data_initialised.database_link.datetime_to_string(service["created_at"])
         msg = f"Services with tag '{tag}': "
         msg += f"{filtered_services}"
         self.disp.log_debug(msg, title)
-        return HCI.success({"msg": filtered_services})
+        return HCI.success(
+            {"services": filtered_services},
+            content_type=CONST.CONTENT_TYPE,
+            headers=self.runtime_data_initialised.json_header
+        )
 
     async def get_recent_services(self, request: Request) -> Response:
-        """_summary_
-
-        Args:
-            request (Request): _description_
-
-        Returns:
-            Response: _description_
+        """
+        The function to get and filter every services by the most recent to the oldest
         """
         title = "get_recent_services"
         token = self.runtime_data_initialised.boilerplate_incoming_initialised.get_token_if_present(
-            request)
+            request
+        )
         self.disp.log_debug(f"Token = {token}", title)
-        if token is None:
-            return HCI.unauthorized({"error": "Authorization required."})
-
+        if not token:
+            return HCI.unauthorized(
+                {"error": "Authorization required."},
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
         service_data = self.runtime_data_initialised.database_link.get_data_from_table(
-            CONST.TAB_SERVICES)
+            CONST.TAB_SERVICES,
+            "*"
+        )
         recent_services = sorted(
-            service_data, key=lambda x: x["created_at"], reverse=True)[:10]
+            service_data, key=lambda x: x["created_at"], reverse=True
+        )[:10]
         if not recent_services:
-            return HCI.not_found({"error": "No recent services found."}, content_type=CONST.CONTENT_TYPE, headers=self.runtime_data_initialised.json_header)
+            return HCI.not_found(
+                {"error": "No recent services found."},
+                content_type=CONST.CONTENT_TYPE,
+                headers=self.runtime_data_initialised.json_header
+            )
+        for i, service in enumerate(recent_services):
+            recent_services[i]["created_at"] = self.runtime_data_initialised.database_link.datetime_to_string(service["created_at"])
         self.disp.log_debug(f"Recent services: {recent_services}", title)
-        return HCI.success({"msg": recent_services})
+        return HCI.success(
+            {"services": recent_services},
+            content_type=CONST.CONTENT_TYPE,
+            headers=self.runtime_data_initialised.json_header
+        )
 
     async def create_service(self, request: Request, name: str) -> Response:
         """
