491e85d105d17394a8d11cdec7bedf17696223e7
Harleen <harleen.singh-kaur@epitech.eu>
2024-10-28 22:56:17 +0100

    [ADD] store data, used to stock the token when user connects to the account

---
+++ app/front/mobile/src/components/StoreData/storeData.tsx
@@ -0,0 +1,36 @@
+// import React, { useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export const storeValue = async (key: string, value: any) => {
+    try {
+        console.log("Set value.")
+        await AsyncStorage.setItem(key, value);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+export const getValue = async (key: string) => {
+    try {
+        const value = await AsyncStorage.getItem(key);
+        if(value !== null) {
+            return value;
+        }
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+export const deleteKey = async (key: string) => {
+    try {
+        await AsyncStorage.removeItem(key);
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+// export default {
+//     storeValue,
+//     getValue,
+//     deleteKey
+// }
