f419450deb27cef4355bd799c9814d6f28499d44
Harleen <harleen.singh-kaur@epitech.eu>
2024-11-01 18:47:51 +0100

    [ADD] Create page Then this button

--- app/front/web/src/components/CreateApplet-Comp/ThenBlock.vue
+++ app/front/web/src/components/CreateApplet-Comp/ThenBlock.vue
@@ -1,95 +1,65 @@
 <template>
-  <div class="container">
+  <div class="thenThisContainer">
     <div class="ifThisBlock">
-      <span>If This</span>
+      <span>Then That</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  props: {
-    action: {
-      type: Object as PropType<{ name: string; description: string; logo: string; color: string } | null>,
-      required: false
-    }
-  },
-  computed: {
-    serviceColor(): string {
-      return this.action && this.action.color ? this.action.color : 'default-gray';
-    }
-  },
   methods: {
     addService() {
-      this.$emit('service-selected', {
-        name: 'Service ABC',
-        description: 'Send a notification',
-        logo: '/path/to/logo.png',
-        color: 'service-green'
-      });
-    },
-    editService() {
-      alert('Edit service');
-    },
-    deleteService() {
-      this.$emit('service-selected', null);
+      alert('Add Service');
     }
   }
 });
 </script>
 
 <style scoped>
-.then-block {
-  background-color: black;
+
+.thenThisContainer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Arial, sans-serif;
+
+  padding: 10px;
+  min-height: 30vh;
+  box-sizing: border-box;
+}
+
+.ifThisBlock {
+  background-color: grey;
   color: white;
-  padding: 10px 5vw;
+  padding: 10px 6vw;
   border-radius: 75px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   gap: 5vw;
   max-width: 90%;
   flex-wrap: wrap;
-  text-align: center;
-  font-family: Arial, sans-serif;
 }
 
-.then-block button {
-  background-color: white;
-  color: black;
-  border-radius: 20px;
-  padding: 1vw 1vw;
-  font-size: 20px;
+.ifThisBlock span {
   font-weight: bold;
-  border: none;
-  cursor: pointer;
-  margin-top: 10px;
-}
-
-.default-gray {
-  background-color: #ccc;
-}
-
-.service-green {
-  background-color: #4caf50;
+  font-size: 5vw;
+  white-space: nowrap;
 }
 
 @media (max-width: 600px) {
-  .then-block {
+  .ifThisBlock {
     padding: 8px 3vw;
     gap: 1vw;
   }
 
-  .then-block h3 {
+  .ifThisBlock span {
     font-size: 6vw;
   }
 
-  .then-block button {
-    padding: 2vw 4vw;
-    font-size: 4vw;
-  }
-
 }
+
 </style>
