51f4cf09abe7b875b69846aa57b9d020196b216b
Hanra <37299738+HenraL@users.noreply.github.com>
2024-11-03 11:35:21 +0100

    [UPDATE] start_server to use the docker system environement then the local if present

--- app/back/server/start_server.sh
+++ app/back/server/start_server.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-. ./server_env/bin/activate
+if [[ -x "${SYSTEM_ENV_LOCATION}/${SYSTEM_ENV_NAME}/bin/activate" ]]; then
+    . ${SYSTEM_ENV_LOCATION}/${SYSTEM_ENV_NAME}/bin/activate
+elif [[ -x ". ./server_env/bin/activate" ]]; then
+    . ./server_env/bin/activate
+else
+    make create_environement install_dependencies
+    if [[ -x ". ./server_env/bin/activate" ]]; then
+        . ./server_env/bin/activate
+    fi
+    echo "Error: Unable to activate environment"
+fi
+
 python3 ./src/ \
     --host 0.0.0.0 \
     --port 5000 \
