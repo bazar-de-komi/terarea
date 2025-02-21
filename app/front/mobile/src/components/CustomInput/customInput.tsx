2f14c625f793f0bd89d851c6624eadbe631f36b9
Eric <eric1.xu@epitech.eu>
2025-02-21 18:35:21 +0100

    [UPDATE] input to old style

--- app/front/mobile/src/components/CustomInput/customInput.tsx
+++ app/front/mobile/src/components/CustomInput/customInput.tsx
@@ -18,31 +18,14 @@ const styles = StyleSheet.create({
     customInputContainer: {
         backgroundColor: 'white',
         width: '70%',
-        height: 30,
 
         borderColor: '#e8e8e8',
         borderWidth: 1,
-        borderRadius: 10,
+        borderRadius: 5,
 
         paddingHorizontal: 10,
-        justifyContent: 'center',
         marginVertical: 5,
-    }
+    },
 })
-
-// const styles = StyleSheet.create({
-//     CustomInputContainer: {
-//         backgroundColor: 'white',
-//         width: '70%',
-//         height: 26,
-
-//         borderColor: '#e8e8e8',
-//         borderRadius: 10,
-
-//         paddingHorizontal: 10,
-//         paddingTop: 4,
-//         marginVertical: 5,
-//     },
-// })
 
 export default CustomInput
